c86fde6284a6a82cd524bfb5928a6eabd91180d3
ajakovac <antal.jakovac@gmail.com>
2023-04-04 17:58:55 +0200

    robot control works

--- communication/robot_control_server.py
+++ communication/robot_control_server.py
@@ -2,6 +2,7 @@ import socket
 import json
 import keyboard
 import numpy as np
+import time
 
 # Set up a TCP/IP server
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,35 +13,39 @@ tcp_socket.bind(server_address)
 tcp_socket.listen(1)
 print("Waiting for connection")
 connection, client = tcp_socket.accept()
+print(f'connected to {client}')
 message = 'start'
 
-speed = np.array([0,0])
-kin = ''
+speed  = np.array([0,0], dtype=float)
+kin = ' '
 while kin != 'q':
     kin = keyboard.read_key()
     if kin == 'up' :
-        dv = [1,1]
+        dv = np.array([1,0], dtype=float)
     elif kin == 'down' :
-        dv = [-1,-1]
+        dv = np.array([-1,0], dtype=float)
     elif kin == 'right' :
-        dv = [-1,1]
+        dv = np.array([0,1], dtype=float)
     elif kin == 'left' :
-        dv = [-1,1]
+        dv = np.array([0,-1], dtype=float)
+    elif kin == 'space' :
+        dv = -speed
     elif kin == 'q' :
-        dv = [0,0]
+        dv = np.array([0,0], dtype=float)
     else:
         continue
-    speed = np.array([ min( max(speed[i] + dv[i] ,0),100) for i in range(2) ])
+    speed = np.array([ min( max(speed[i] + dv[i] ,-100.0),100.0) for i in range(2) ])
     print(f'speed = {speed}')
-    dict = {
+    dd = {
         'message' : kin,
-        'speed' : speed
+        'speed' : list(speed)
     }
-    try:
-        connection.sendall(json.dumps(dict).encode('utf-8'))
-    except:
-        print('send failed')
+    connection.sendall(json.dumps(dd).encode('utf-8'))
+    time.sleep(0.1)
+#    except:
+#        print('send failed')
 
 
 connection.close()
 tcp_socket.close()
+ 
