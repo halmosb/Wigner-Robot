c725ace364b23d8fb5d4e0245379fa5d4a6a22bf
ajakovac <antal.jakovac@gmail.com>
2023-06-29 19:43:00 +0200

    Fix speech

--- system/speech.py
+++ system/speech.py
@@ -4,10 +4,10 @@ from threading import Thread
 class TTSpeech:
 
     def __init__(self):
+        self.engine = pyttsx3.init()
         self.engine.setProperty('rate', 200)
         self.engine.setProperty('volume', 1)
         self.engine.setProperty('voice', "hungarian")
-        self.engine = pyttsx3.init()
     
     def say(self, message):
         self.engine.say(message)
